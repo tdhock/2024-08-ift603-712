55b1808fb2aa3f24ca77761c16a7b44974da0cdc
Toby Dylan Hocking <toby.dylan.hocking@usherbrooke.ca>
2024-09-16 14:39:36 -0400

    cv noise samples unused duration

--- cv-noise-samples.R
+++ cv-noise-samples.R
@@ -4,51 +4,63 @@ max.N <- 1000
 N <- max.N*n.folds/(n.folds-1)
 abs.x <- 3*pi
 set.seed(1)
+norm01 <- function(z,ref=z)(z-min(ref))/(max(ref)-min(ref))
+(grid.dt <- data.table(
+  raw=seq(-abs.x,abs.x, l=201),
+  y=0 #for mlr3
+)[, x := norm01(raw)][])
 x.vec <- runif(N, -abs.x, abs.x)
 str(x.vec)
 reg.pattern.list <- list(
   sin=sin,
   constant=function(x)0)
 standard.deviation.vec <- c(
-  easy=0.1,
+  easy=0.4,
   hard=1.1,
   impossible=5)
 reg.task.list <- list()
 reg.data.list <- list()
-norm01 <- function(z,ref=z)(z-min(ref))/(max(ref)-min(ref))
-for(difficulty in names(standard.deviation.vec)){
-  standard.deviation <- standard.deviation.vec[[difficulty]]
-  for(signal in names(reg.pattern.list)){ # HI BASIL
+grid.signal.dt.list <- list()
+for(signal in names(reg.pattern.list)){
+  f <- reg.pattern.list[[signal]]
+  for(difficulty in names(standard.deviation.vec)){
+    standard.deviation <- standard.deviation.vec[[difficulty]]
     task_id <- paste(signal, difficulty)
-    f <- reg.pattern.list[[signal]]
     signal.vec <- f(x.vec)
     y <- signal.vec+rnorm(N,sd=standard.deviation)
     task.dt <- data.table(
-      x=norm01(x.vec),
+      x=norm01(x.vec,grid.dt$raw),
       y = norm01(y))
     reg.data.list[[paste(difficulty, task_id)]] <- data.table(
       difficulty,
       signal,
       task_id,
-      algorithm="signal",
-      signal.vec=norm01(signal.vec,y),
       task.dt)
     reg.task.list[[paste(difficulty, task_id)]] <- mlr3::TaskRegr$new(
       task_id, task.dt, target="y"
     )
+    grid.signal.dt.list[[paste(difficulty, task_id)]] <- data.table(
+      difficulty,
+      signal,
+      task_id,
+      algorithm="ideal",
+      x=grid.dt$x,
+      y=norm01(f(grid.dt$raw),y)
+    )      
   }
 }
 (reg.data <- rbindlist(reg.data.list))
-
+(grid.signal.dt <- rbindlist(grid.signal.dt.list))
 if(require(animint2)){
   ggplot()+
     geom_point(aes(
       x, y),
       data=reg.data)+
     geom_line(aes(
-      x, signal.vec),
+      x, y),
       color="red",
-      data=reg.data)+
+      size=2,
+      data=grid.signal.dt)+
     facet_grid(signal ~ difficulty, labeller=label_both)
 }
 
@@ -57,7 +69,7 @@ n.train.sizes <- 9
 reg_size_cv$param_set$values$train_sizes <- n.train.sizes
 reg_size_cv$param_set$values$folds <- n.folds
 reg_size_cv$param_set$values$random_seeds <- 1
-reg_size_cv
+reg_size_cv$instantiate(reg.task.list[[1]])#required for consistent folds across tasks.
 
 (reg.learner.list <- list(
   if(requireNamespace("rpart"))mlr3::LearnerRegrRpart$new(),
@@ -84,48 +96,50 @@ reg.bench.score <- nc::capture_first_df(
 ][]
 train_size_vec <- unique(reg.bench.score$train_size)
 
-grid.dt <- data.table(x=seq(0,1, l=101), y=0)
 grid.task <- mlr3::TaskRegr$new("grid", grid.dt, target="y")
 pred.dt.list <- list()
 point.dt.list <- list()
-signal.dt.list <- list()
 for(score.i in 1:nrow(reg.bench.score)){
   reg.bench.row <- reg.bench.score[score.i]
   task.dt <- data.table(
     reg.bench.row$task[[1]]$data(),
     reg.bench.row$resampling[[1]]$instance$id.dt)
   set.ids <- data.table(
-    set.name=c("test","train")
+    Set=c("test","train")
   )[
-  , data.table(row_id=reg.bench.row[[set.name]][[1]])
-  , by=set.name]
+  , data.table(row_id=reg.bench.row[[Set]][[1]])
+  , by=Set]
   i.points <- set.ids[
     task.dt, on="row_id"
   ][
-    is.na(set.name), set.name := "unused"
+    is.na(Set), Set := "unused"
   ]
-  point.dt.list[[score.i]] <- data.table(
-    reg.bench.row[, .(signal_difficulty_Ntrain, test.fold)],
+  point.id <- reg.bench.row[, paste(signal_difficulty_Ntrain, test.fold, algorithm)]
+  point.dt.list[[point.id]] <- data.table(
+    reg.bench.row[, .(signal_difficulty_Ntrain, test.fold, algorithm)],
     i.points)
   i.learner <- reg.bench.row$learner[[1]]
   pred.dt.list[[score.i]] <- data.table(
     reg.bench.row[, .(
-      signal_difficulty_Ntrain, test.fold, algorithm
+      signal_difficulty_Ntrain, signal, difficulty, train_size, test.fold, algorithm
     )],
     as.data.table(
       i.learner$predict(grid.task)
     )[, .(x=grid.dt$x, y=response)]
   )
-  signal.dt.list[[score.i]] <- reg.data[reg.bench.row, on=.(signal, difficulty)]  
 }
 (pred.dt <- rbindlist(pred.dt.list))
-(point.dt <- rbindlist(point.dt.list))
-(signal.dt <- rbindlist(signal.dt.list))
+(point.dt <- rbindlist(point.dt.list)[algorithm=="featureless"])
+(upred <- unique(pred.dt[, .(signal_difficulty_Ntrain, signal, difficulty, train_size)]))
+signal.dt <- upred[
+  grid.signal.dt, on=.(signal,difficulty), allow.cartesian=TRUE]
+
+point.dt[grepl(" 10$", signal_difficulty_Ntrain) & test.fold==1 & Set=="train"][, .SD[1:2], by=.(signal_difficulty_Ntrain)]
 
 algo.colors <- c(
   featureless="red",
-  rpart="blue",
-  signal="grey50")
+  rpart="deepskyblue",
+  ideal="black")
 algo.sizes <- c(
   truth=6,
   featureless=4,
@@ -145,36 +159,56 @@ test.proposed <- reg.bench.test[, {
   paired <- t.test(rpart, featureless, alternative="two.sided", paired=TRUE)
   unpaired <- t.test(rpart, featureless, alternative="two.sided", paired=FALSE)
   data.table(
-    mean.of.diff=paired$estimate, p.paired=paired$p.value,
+    mean.of.diff=paired$estimate, p.paired=paired$p.value, p.value=unpaired$p.value,
     mean.rpart=unpaired$estimate[1], mean.featureless=unpaired$estimate[2], p.unpaired=unpaired$p.value)
 }, keyby=.(signal,difficulty,train_size,signal_difficulty_Ntrain)
 ][, `:=`(
   difference=ifelse(
-    is.nan(p.paired) | p.paired>0.05, "not significant", "significant"),
+    is.nan(p.value) | p.value>0.05, "not significant", "significant"),
   xmin=10^(rect.x-seq.diff),
   xmax=10^(rect.x+seq.diff)
 ), by=.(signal,difficulty)][]
-test.proposed[difficulty=="hard" & signal=="sin"]
+if(FALSE){#bug in R?
+  reg.bench.test[difficulty=="hard" & train_size==1000 & signal=="constant", t.test(featureless, rpart, paired=TRUE)]
+  reg.bench.test[difficulty=="hard" & train_size==1000 & signal=="constant", t.test(featureless, rpart, paired=FALSE)]
+  dput(data.frame(reg.bench.test[difficulty=="hard" & train_size==1000 & signal=="constant", .(err1=featureless, err2=rpart)]), control="digits17")
+  err1 = c(-1.6076199373862132, -1.658521185520103, -1.6549424312339873, -1.5887767975086149, -1.634129577540383, -1.7442711937982249)
+  err2 = c(-1.6076199373862132, -1.6585211855201032, -1.6549424312339875, -1.5887767975086149, -1.6341295775403832, -1.7442711937982252)
+  t.test(err1,err2,paired=TRUE)
+  t.test(err1,err2,paired=FALSE)
+}
+test.proposed[difficulty=="hard" & train_size==1000]
 reg.bench.join <- reg.bench.wide[
   test.proposed[, .(signal_difficulty_Ntrain,signal,difficulty,train_size,difference)],
   on=.NATURAL]
 mid.x <- 10^((max(rect.x)+min(rect.x))/2)
-lim <- c(0.005, 0.5)
-
+data.color <- "grey50"
+mse.limits <- c(0.01, 0.045)
+mse.breaks <- c(0.01,0.02,0.04)
+Toff <- 1.2
+Tbrk <- c(0,0.5,1)
+Tbreaks <- c(Tbrk,Tbrk+Toff)
+Tlabels <- c(Tbrk,Tbrk)
+unused.x <- 1.1
+unused.y.point <- 0.1
+unused.y.text <- 0
+Tpred <- function(DT){
+  if(! "Set" %in% names(DT)){
+    DT <- data.table(Set=c("train","test"))[, data.table(DT), by=Set]
+  }
+  data.table(DT)[
+  , x := ifelse(Set=="test",0,Toff)+x
+  ][
+    Set=="unused", `:=`(x=unused.x, y=unused.y.point)
+  ][]
+}
+(data.sizes <- point.dt[, .(N=.N), by=.(signal_difficulty_Ntrain, test.fold, Set)])
 viz <- animint(
   title="Samples required to learn non-trivial regression model",
   overview=ggplot()+
     ggtitle("Select signal, difficulty, Ntrain")+
     theme_bw()+
     theme_animint(width=600, height=300)+
-    geom_rect(aes(
-      xmin=xmin, xmax=xmax,
-      ymin=0, ymax=Inf),
-      alpha=0.1,
-      fill="black",
-      color=NA,
-      clickSelects="signal_difficulty_Ntrain",
-      data=test.proposed)+
     geom_ribbon(aes(
       train_size,
       ymin=regr.mse_mean-regr.mse_sd,
@@ -197,35 +231,50 @@ viz <- animint(
       data=reg.bench.join)+
     geom_segment(aes(
       train_size, 10^mean.rpart,
+      key=1,
       xend=train_size, yend=10^mean.featureless),
       showSelected="signal_difficulty_Ntrain",
       size=3,
       alpha=0.5,
       data=test.proposed)+
     geom_text(aes(
-      train_size, 10^pmax(mean.rpart,mean.featureless)*1.2,
+      train_size, 10^pmax(mean.rpart,mean.featureless)*1.1,
+      key=1,
       hjust=ifelse(train_size<mid.x, 0, 1),
       label=fcase(
-        p.paired<0.0001, "p<0.0001",
-        is.nan(p.paired), "Diff=0",
-        default=sprintf("p=%.4f", p.paired))),
+        p.value<0.0001, "p<0.0001",
+        is.nan(p.value), "Diff=0",
+        default=sprintf("p=%.4f", p.value))),
       showSelected="signal_difficulty_Ntrain",
+      data=test.proposed)+
+    geom_rect(aes(
+      xmin=xmin, xmax=xmax,
+      ymin=0, ymax=Inf),
+      alpha=0.1,
+      fill="black",
+      color=NA,
+      clickSelects="signal_difficulty_Ntrain",
       data=test.proposed)+
     scale_y_log10(
       "Mean Squared Error (log scale)",
-      limits=c(0.007, 0.12))+
-    scale_x_log10()+
+      limits=mse.limits,
+      breaks=mse.breaks
+    )+
+    scale_x_log10(
+      "Ntrain = Number of samples in train set (log scale)")+
     facet_grid(signal ~ difficulty, labeller=label_both),
   scatter=ggplot()+
     ggtitle("MSE for selected")+
     theme_bw()+
-    theme_animint(width=400, height=300)+
+    theme_animint(width=300, height=300)+
     theme(legend.position="none")+
-    coord_equal(xlim=lim, ylim=lim)+
+    coord_equal(xlim=mse.limits, ylim=mse.limits)+
     scale_x_log10(
-      "featureless")+
+      "featureless (log scale)",
+      breaks=mse.breaks)+
     scale_y_log10(
-      "rpart")+
+      "rpart (log scale)",
+      breaks=mse.breaks)+
     geom_abline(aes(
       slope=slope, intercept=intercept),
       color="grey50",
@@ -240,7 +289,9 @@ viz <- animint(
       size=5)+
     scale_color_manual(values=algo.colors)+
     geom_point(aes(
-      10^featureless, 10^rpart, tooltip=sprintf(
+      10^featureless, 10^rpart,
+      key=test.fold,
+      tooltip=sprintf(
         "fold %d featureless=%.3f rpart=%.3f", test.fold, featureless, rpart)),
       showSelected="signal_difficulty_Ntrain",
       clickSelects="test.fold",
@@ -254,10 +305,13 @@ viz <- animint(
     theme(legend.position="none")+
     scale_y_discrete("Algo")+
     scale_x_log10(
-      "Mean Squared Error (log scale)")+
+      "Mean Squared Error (log scale)",
+      limits=mse.limits,
+      breaks=mse.breaks)+
     scale_color_manual(values=algo.colors)+
     geom_point(aes(
       regr.mse, algorithm,
+      key=paste0(algorithm, test.fold),
       color=algorithm,
       tooltip=sprintf(
         "%s fold %d MSE=%.3f", algorithm, test.fold, regr.mse)),
@@ -266,39 +320,101 @@ viz <- animint(
       alpha=0.7,
       size=5,
       data=reg.bench.score),
+  ## pred=ggplot()+
+  ##   ggtitle("Predictions for selected train/test split")+
+  ##   theme_bw()+
+  ##   theme_animint(height=300, width=900)+
+  ##   geom_point(aes(
+  ##     x, y,
+  ##     key=paste(signal_difficulty_Ntrain, row_id)),
+  ##     showSelected=c("signal_difficulty_Ntrain","test.fold"),
+  ##     size=3,
+  ##     fill="white",
+  ##     color=data.color,
+  ##     data=point.dt[Set!="unused"])+
+  ##   scale_x_continuous("x = feature/input")+
+  ##   scale_y_continuous("y = label/output")+
+  ##   scale_size_manual(values=algo.sizes)+
+  ##   scale_color_manual(values=algo.colors)+
+  ##   geom_line(aes(
+  ##     x, y,
+  ##     key=algorithm,
+  ##     color=algorithm,
+  ##     size=algorithm,
+  ##     group=algorithm),
+  ##     showSelected=c("signal_difficulty_Ntrain","test.fold"),
+  ##     data=pred.dt)+
+  ##   geom_line(aes(
+  ##     x, y,
+  ##     key=algorithm,
+  ##     color=algorithm,
+  ##     size=algorithm),
+  ##     showSelected=c("signal_difficulty_Ntrain"),
+  ##     data=signal.dt)+
+  ##   geom_text(aes(
+  ##     0, 0.98,
+  ##     key=Set,
+  ##     label=paste0("N",Set,"=",N)),
+  ##     hjust=0,
+  ##     data=data.sizes,
+  ##     color=data.color,
+  ##     showSelected="signal_difficulty_Ntrain")+
+  ##   facet_grid(
+  ##     . ~ Set,
+  ##     labeller=label_both),
   pred=ggplot()+
     ggtitle("Predictions for selected train/test split")+
     theme_bw()+
-    theme_animint(height=300, width=1000)+
+    theme_animint(height=300, width=900)+
     geom_point(aes(
-      x, y),
+      x, y,
+      key=row_id),
       showSelected=c("signal_difficulty_Ntrain","test.fold"),
       size=3,
       fill="white",
-      color="black",
-      data=point.dt[set.name!="unused"][, Set := set.name])+
-    scale_size_manual(values=algo.sizes)+
-    scale_color_manual(values=algo.colors)+
-    geom_line(aes(
-      x, signal.vec, color=algorithm, size=algorithm),
-      showSelected=c("signal_difficulty_Ntrain"),
-      data=signal.dt)+
+      color=data.color,
+      data=Tpred(point.dt))+
+    scale_x_continuous(
+      "x = feature/input",
+      labels=Tlabels,
+      breaks=Tbreaks)+
+    scale_y_continuous("y = label/output")+
     geom_line(aes(
       x, y,
+      key=paste(algorithm,Set),
       color=algorithm,
       size=algorithm,
-      group=algorithm),
+      group=paste(algorithm,Set)),
       showSelected=c("signal_difficulty_Ntrain","test.fold"),
-      data=pred.dt)+
-    facet_grid(
-      . ~ Set,
-      labeller=label_both),
+      data=Tpred(pred.dt))+
+    geom_line(aes(
+      x, y,
+      key=Set,
+      group=Set,
+      color=algorithm,
+      size=algorithm),
+      showSelected=c("signal_difficulty_Ntrain"),
+      data=Tpred(signal.dt))+
+    geom_text(aes(
+      x, ifelse(Set=="unused", unused.y.text, 0.98),
+      hjust=ifelse(Set=="unused", 0.5, 0),
+      key=Set,
+      label=paste0(Set," set N=",N)),
+      data=Tpred(data.sizes[, x := 0]),
+      color=data.color,
+      showSelected="signal_difficulty_Ntrain")+
+    scale_size_manual(values=algo.sizes)+
+    scale_color_manual(values=algo.colors),
   out.dir="cv-noise-samples",
   source="https://github.com/tdhock/2024-08-ift603-712/blob/main/cv-noise-samples.R",
+  duration=list(
+    test.fold=1000,
+    signal_difficulty_Ntrain=1000),
   first=list(
     signal_difficulty_Ntrain="sin easy 1000")
 )
 if(FALSE){
-  animint2pages(viz, "2024-09-15-K-fold-CV-train-sizes-regression")
+  animint2pages(viz, "2024-09-16-K-fold-CV-train-sizes-regression")
 }
+viz
 
